9d625558bb422be7316b7796d7315c25c7fad3bb
Gunter-R <vvrudavskiy_2@edu.hse.ru>
2023-05-11 20:01:22 +0300

    Add plugin mvp

--- main.ts
+++ main.ts
@@ -1,137 +1,105 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, FileSystemAdapter, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import * as child from 'child_process';
+import * as http from "http";
+import * as path from "path";
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class LatexSympyCalculatorPlugin extends Plugin {
+	pythonServer: child.ChildProcessWithoutNullStreams;
 
 	async onload() {
-		await this.loadSettings();
+		this.pythonServer = await child.spawn('python3', [this.getServerPath()]);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'calculate-latex',
+			name: 'Calculate latex expression',
+			editorCallback: this.calculatorFactory(ServerOption.Latex),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+			id: 'factor',
+			name: 'Factor',
+			editorCallback: this.calculatorFactory(ServerOption.Factor),
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: 'Numerical',
+			name: 'Numerical',
+			editorCallback: this.calculatorFactory(ServerOption.Numerical),
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addCommand({
+			id: 'expand',
+			name: 'Expand',
+			editorCallback: this.calculatorFactory(ServerOption.Expand),
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addCommand({
+			id: 'get-matrix-raw-echelon-form',
+			name: 'Get matrix raw echelon form',
+			editorCallback: this.calculatorFactory(ServerOption.MatrixRawEchelonForm),
+		});
 	}
 
 	onunload() {
-
+		this.pythonServer.kill('SIGINT');
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	calculatorFactory(option: ServerOption) {
+		return (editor: Editor, view: MarkdownView) => {
+			post(editor.getSelection(), option,
+				(data: string) => editor.replaceSelection(data),
+				(err) => new Notice(err));
+		}
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getServerPath() {
+		const adapter = app.vault.adapter;
+		if (adapter instanceof FileSystemAdapter) {
+			return path.join(adapter.getBasePath(),
+				'.obsidian', 'plugins', 'obsidian-latex-calculator', 'sympy-server.py');
+		}
+		return "";
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+enum ServerOption {
+	Latex = '/latex',
+	MatrixRawEchelonForm ='/matrix-raw-echelon-form',
+	Factor = '/factor',
+	Numerical = '/numerical',
+	Expand = '/expand',
+}
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+function post(data: string, path: ServerOption, onSuccess: Function, onError: Function) {
+	const _data = JSON.stringify({data: data});
 
-	display(): void {
-		const {containerEl} = this;
+	const options = {
+		hostname: '127.0.0.1',
+		port: 7395,
+		path: path,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': _data.length,
+		},
+	};
 
-		containerEl.empty();
+	const req = http.request(options, res => {
+		res.on('data', data => {
+			const result = JSON.parse(data);
+			if (result.error) {
+				onError(result.error);
+			} else {
+				onSuccess(result.data);
+			}
+		});
+	});
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+	req.on('error', () => {
+		console.log('Activating the server...\nPlease retry for a moment.');
+	});
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	req.write(_data);
+	req.end();
 }
